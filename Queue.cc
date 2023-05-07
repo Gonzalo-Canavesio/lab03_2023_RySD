e48c2ffd532d0a47ae53ba324719a02c1ce31a26
Lautaro Bachmann <laurigbach@gmail.com>
2023-05-07 20:37:13 -0300

    Revertir cambios hechos a Queue por accidente

--- Queue.cc
+++ Queue.cc
@@ -8,9 +8,12 @@ using namespace omnetpp;
 
 class Queue: public cSimpleModule {
 private:
+    cOutVector bufferSizeQueue;
+    cOutVector packetDropQueue;
     cQueue buffer;
     cMessage *endServiceEvent;
     simtime_t serviceTime;
+    void send();
 public:
     Queue();
     virtual ~Queue();
@@ -32,9 +35,23 @@ Queue::~Queue() {
 
 void Queue::initialize() {
     buffer.setName("buffer");
+    bufferSizeQueue.setName("bufferSizeQueue");
+    packetDropQueue.setName("packetDropQueue");
+    packetDropQueue.record(0);
     endServiceEvent = new cMessage("endService");
 }
 
+void Queue::send(){
+    if (!buffer.isEmpty()) {
+            // dequeue packet
+            cMessage *pkt = (cMessage*) buffer.pop();
+            // send packet
+            send(pkt, "out");
+            // start new service
+            serviceTime = pkt->getDuration();
+            scheduleAt(simTimed() + serviceTime, endServiceEvent);
+    }
+}
 void Queue::finish() {
 }
 
@@ -43,22 +60,22 @@ void Queue::handleMessage(cMessage *msg) {
     // if msg is signaling an endServiceEvent
     if (msg == endServiceEvent) {
         // if packet in buffer, send next one
-        if (!buffer.isEmpty()) {
-            // dequeue packet
-            cPacket *pkt = (cPacket*) buffer.pop();
-            // send packet
-            send(pkt, "out");
-            // start new service
-            serviceTime = pkt->getDuration();
-            scheduleAt(simTime() + serviceTime, endServiceEvent);
-        }
+        send();
     } else { // if msg is a data packet
-        // enqueue the packet
-        buffer.insert(msg);
-        // if the server is idle
-        if (!endServiceEvent->isScheduled()) {
-            // start the service
-            scheduleAt(simTime(), endServiceEvent);
+        if (buffer.getLength() >= par("bufferSize").longValue()){
+            //drop the packet
+            delete msg;
+            this->bubble("Packet dropped");
+            packetDropQueue.record(1);
+        } else{ 
+            // enqueue the packet
+            buffer.insert(msg);
+            bufferSizeQueue.record(buffer.getLength());
+            // if the server is idle
+            if (!endServiceEvent->isScheduled()) {
+                // start the service
+                scheduleAt(simTime() + 0, endServiceEvent);
+            }
         }
     }
 }
